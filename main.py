9aedbbcd36c784409c7b22bbf0a2902bc508713a
greenrose7 <lucascevans@live.com>
2025-01-15 18:17:37 -0400

    Instantiate and render player triangle

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -14,11 +15,14 @@ def main():
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
