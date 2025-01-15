32341b19ae9e9869ea9ff2a7d093d219e8086495
greenrose7 <lucascevans@live.com>
2025-01-15 18:02:11 -0400

    Added Clock/framerate limiter

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     # print("Starting asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,6 +20,7 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
